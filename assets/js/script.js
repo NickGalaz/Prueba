1a6ba37b0adae35b5158e5aa562d4ac01579717a
Nick <galaznicolas12@gmail.com>
2021-12-02 20:22:18 -0300

    Cambio  SCRIPT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 
 // Smooth Scroll y tooltip
-$(document).ready(function() {
-    $("a").click(function(event) {
+$(document).ready(function () {
+    $("a").click(function (event) {
         event.preventDefault();
         var gato = this.hash;
-       
+
         $("html, body").animate({
             scrollTop: $(gato).offset().top - 70,
         }, 1000);
     });
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 });
 
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
