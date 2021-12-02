7fbdd262682d4c538138490c8bf476bfdde244d1
Nick <galaznicolas12@gmail.com>
2021-12-02 19:50:35 -0300

    Inicio

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+// Smooth Scroll y tooltip
+$(document).ready(function() {
+    $("a").click(function(event) {
+        event.preventDefault();
+        var gato = this.hash;
+       
+        $("html, body").animate({
+            scrollTop: $(gato).offset().top - 70,
+        }, 1000);
+    });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
